2eb79434f25e1005d79e8761323b0172f84a6fcf
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-03-03 12:50:44 +0100

    add: UnitTests

---
+++ Tests/Runtime/XRPropertiesTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.XR.Interaction.Toolkit;
+using Innoactive.Creator.SceneObjects.Properties;
+using Innoactive.Hub.Unity.Tests.Training;
+
+namespace Innoactive.Creator.XR.Tests
+{
+    public class XRPropertiesTests : RuntimeTests
+    {
+        private bool wasTouched;
+        private bool wasUnTouched;
+        
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            XRTestUtilities.CreateInteractionManager();
+
+            wasTouched = false;
+            wasUnTouched = false;
+        }
+        
+        [UnityTest]
+        public IEnumerator XRTouchablePropertyTest()
+        {
+            XRGrabInteractable interactable = XRTestUtilities.CreateGrabInteractable();
+            XR_TouchableProperty touchProperty = interactable.gameObject.AddComponent<XR_TouchableProperty>();
+            
+            touchProperty.Touched += OnTouched;
+            touchProperty.Untouched += OnUntouched;
+            
+            Assert.IsFalse(touchProperty.IsBeingTouched);
+            
+            XRDirectInteractor interactor = XRTestUtilities.CreateDirectInteractor();
+            
+            yield return new WaitUntil(()=> wasTouched);
+            
+            Assert.IsTrue(touchProperty.IsBeingTouched);
+            
+            interactor.transform.position = Vector3.up * 100;
+            
+            yield return new WaitUntil(()=> wasUnTouched);
+            
+            Assert.IsFalse(touchProperty.IsBeingTouched);
+
+            touchProperty.Touched -= OnTouched;
+            touchProperty.Untouched -= OnUntouched;
+        }
+        
+        [UnityTest]
+        public IEnumerator XRTouchablePropertyFastForwardTouchTest()
+        {
+            XRGrabInteractable interactable = XRTestUtilities.CreateGrabInteractable();
+            XR_TouchableProperty touchProperty = interactable.gameObject.AddComponent<XR_TouchableProperty>();
+            
+            touchProperty.Touched += OnTouched;
+            touchProperty.Untouched += OnUntouched;
+            
+            Assert.IsFalse(touchProperty.IsBeingTouched);
+            
+            touchProperty.FastForwardTouch();
+            
+            yield return new WaitUntil(()=> wasTouched && wasUnTouched);
+            
+            Assert.IsFalse(touchProperty.IsBeingTouched);
+            
+            touchProperty.Touched -= OnTouched;
+            touchProperty.Untouched -= OnUntouched;
+        }
+
+        private void OnTouched(object sender, EventArgs e)
+        {
+            wasTouched = true;
+            Debug.LogWarning("OnTouched");
+            Assert.That(sender is XR_TouchableProperty);
+        }
+        
+        private void OnUntouched(object sender, EventArgs e)
+        {
+            wasUnTouched = true;
+            Debug.LogWarning("OnUntouched");
+            Assert.That(sender is XR_TouchableProperty);
+        }
+        
+        
+    }
+}
---
+++ Tests/Runtime/XRTestUtilities.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace Innoactive.Creator.XR.Tests
+{
+    public static class XRTestUtilities
+    {
+        public static XRInteractionManager CreateInteractionManager()
+        {
+            GameObject managerGO = new GameObject("XR Interaction Manager");
+            XRInteractionManager manager = managerGO.AddComponent<XRInteractionManager>();
+            return manager;
+        }
+
+        internal static XRDirectInteractor CreateDirectInteractor()
+        {
+            GameObject interactorGO = new GameObject("XR Interactor");
+            CreateGOSphereCollider(interactorGO);
+            XRDirectInteractor interactor = interactorGO.AddComponent<XRDirectInteractor>();
+            XRController controller = interactorGO.GetComponent<XRController>();
+            controller.enableInputTracking = false;
+            return interactor;
+        }
+        
+        public static XRGrabInteractable CreateGrabInteractable()
+        {
+            GameObject interactableGO = new GameObject("XR Interactable");
+            CreateGOSphereCollider(interactableGO, false);
+            XRGrabInteractable interactable = interactableGO.AddComponent<XRGrabInteractable>();
+            Rigidbody rigidBody = interactableGO.GetComponent<Rigidbody>();
+            rigidBody.useGravity = false;
+            rigidBody.isKinematic = true;
+            return interactable;
+        }
+
+        public static XRRayInteractor CreateRayInteractor()
+        {
+            GameObject interactorGO = new GameObject("Ray Interactor");
+            XRRayInteractor interactor = interactorGO.AddComponent<XRRayInteractor>();
+            XRController controller = interactorGO.GetComponent<XRController>();
+            XRInteractorLineVisual ilv = interactorGO.AddComponent<XRInteractorLineVisual>();
+            controller.enableInputTracking = false;
+            interactor.enableUIInteraction = false;
+            return interactor;
+        }
+        
+        public static XRSocketInteractor CreateSocketInteractor()
+        {
+            GameObject interactorGO = new GameObject("XR Socket Interactor");
+            CreateGOSphereCollider(interactorGO);
+            XRSocketInteractor interactor = interactorGO.AddComponent<XRSocketInteractor>();
+            return interactor;
+        }
+
+        private static void CreateGOSphereCollider(GameObject go, bool isTrigger = true)
+        {
+            SphereCollider collider = go.AddComponent<SphereCollider>();
+            collider.radius = 1.0f;
+            collider.isTrigger = isTrigger;
+        }
+    }
+}
