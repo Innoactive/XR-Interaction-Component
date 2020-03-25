4e49f5e3dc0eeb18edabda364c4278327e648791
Gustavo Quiroz <gusinuhe@gmail.com>
2020-03-25 16:51:21 +0100

    add: XR-Interaction ready to use prefab.
    add: Minimal set of assets for XR-Interactions.
    add: Assembly reference to Creator for loading XR-Interaction-Toolkit
    chg: XRSceneSetup priority.

---
+++ Editor/XRSceneSetup.cs
@@ -0,0 +1,36 @@
+﻿using Innoactive.CreatorEditor.BasicInteraction;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Innoactive.CreatorEditor.XRInteraction
+{
+    /// <summary>
+    /// Scene setup for XR-Interaction.
+    /// </summary>
+    public class XRSceneSetup : OnInteractionFrameworkSetup
+    {
+        /// <inheritdoc />
+        public override void Setup()
+        {
+            SetupXR();
+        }
+        
+        private void SetupXR()
+        {
+            Camera camera = Camera.main;
+            
+            if (camera != null)
+            {
+                Object.DestroyImmediate(camera.gameObject);
+            }
+        
+            GameObject xrSetup = GameObject.Find("[XR_Setup]");
+            
+            if (xrSetup == null)
+            {
+                GameObject prefab = Object.Instantiate(Resources.Load("[XR_Setup]", typeof(GameObject))) as GameObject;
+                prefab.name = prefab.name.Replace("(Clone)", string.Empty);
+            }
+        }
+    }
+}
