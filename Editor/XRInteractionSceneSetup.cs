57558e35fb7d6a635bfd59593a6011a0be7d644c
Gustavo Quiroz <gusinuhe@gmail.com>
2020-04-03 11:47:07 +0200

    chg: XRInteractionSceneSetup was simplified using utilities methods.

--- Editor/XRInteractionSceneSetup.cs
+++ Editor/XRInteractionSceneSetup.cs
@@ -17,20 +17,8 @@ namespace Innoactive.CreatorEditor.XRInteraction
         
         private void SetupXR()
         {
-            Camera camera = Camera.main;
-            
-            if (camera != null)
-            {
-                Object.DestroyImmediate(camera.gameObject);
-            }
-        
-            GameObject xrSetup = GameObject.Find("[XR_Setup]");
-            
-            if (xrSetup == null)
-            {
-                GameObject prefab = Object.Instantiate(Resources.Load("[XR_Setup]", typeof(GameObject))) as GameObject;
-                prefab.name = prefab.name.Replace("(Clone)", string.Empty);
-            }
+            RemoveMainCamera();
+            SetupPrefab("[XR_Setup]");
         }
     }
 }
