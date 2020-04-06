6df6cae64cfacb68818479e6f30205e716f108cf
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-04-06 15:30:32 +0200

    Implementation of the Dependency Manager (#6)
    * chg: XRInteractionPackageValidator was replaced for XRInteractionPackageEnabler which is being used by the DependencyManager.

---
+++ Editor/PackageDependencies/XRInteractionPackageEnabler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEditor;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Innoactive.CreatorEditor.PackageManager.XRInteraction
+{
+    /// <summary>
+    /// Adds Unity's XR-Interaction-Toolkit package as a dependency and sets specified symbol for script compilation.
+    /// </summary>
+    public class XRInteractionPackageEnabler : Dependency
+    {
+        /// <inheritdoc/>
+        public override string Package { get; } = "com.unity.xr.interaction.toolkit";
+
+        /// <inheritdoc/>
+        public override int Priority { get; } = 4;
+        
+        private const string CreatorXRInteractionSymbol = "CREATOR_XR_INTERACTION";
+
+        public XRInteractionPackageEnabler()
+        {
+            OnPackageEnabled += PostProcess;
+        }
+
+        private void PostProcess(object sender, EventArgs e)
+        {
+            BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
+            BuildTargetGroup buildTargetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget);
+            List<string> symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup).Split(';').ToList();
+        
+            if (symbols.Contains(CreatorXRInteractionSymbol) == false)
+            {
+                symbols.Add(CreatorXRInteractionSymbol);
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, string.Join(";", symbols.ToArray()));
+            }
+            
+            OnPackageEnabled -= PostProcess;
+        }
+    }
+}
