b8b8b04f7b67ef67ccb654ce6b344272a2fd3d95
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-08-21 19:47:37 +0200

    feat: new precision grab for interactors TRNG-831 (#40)

--- Runtime/Interaction/DirectInteractor.cs
+++ Runtime/Interaction/DirectInteractor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine.XR.Interaction.Toolkit;
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace Innoactive.Creator.XRInteraction
 {
@@ -10,7 +11,33 @@ namespace Innoactive.Creator.XRInteraction
     /// <remarks>Adds extra control over applicable interactions.</remarks>
     public class DirectInteractor : XRDirectInteractor
     {
+        [SerializeField]
+        private bool precisionGrab = true;
+
+        /// <summary>
+        /// Toggles precision grab on this interactor.
+        /// </summary>
+        public bool PrecisionGrab
+        {
+            get { return precisionGrab; }
+            set
+            {
+                attachTransform.localPosition = initialAttachPosition;
+                attachTransform.localRotation = initialAttachRotation;
+                precisionGrab = value;
+            }
+        }
+
+        private Vector3 initialAttachPosition;
+        private Quaternion initialAttachRotation;
         private bool forceGrab;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            initialAttachPosition = attachTransform.localPosition;
+            initialAttachRotation = attachTransform.localRotation;
+        }
 
         /// <summary>
         /// Gets whether the selection state is active for this interactor. This will check if the controller has a valid selection
@@ -36,6 +63,33 @@ namespace Innoactive.Creator.XRInteraction
         public virtual void AttemptGrab()
         {
             forceGrab = true;
+        }
+        
+        /// <summary>
+        /// This method is called when the interactor first initiates selection of an interactable.
+        /// </summary>
+        /// <param name="interactable">Interactable that is being selected.</param>
+        protected override void OnSelectEnter(XRBaseInteractable interactable)
+        {
+            InteractableObject interactableObject = interactable as InteractableObject;
+            
+            if (precisionGrab && interactableObject.attachTransform == null)
+            {
+                switch (interactableObject.movementType)
+                {
+                    case XRBaseInteractable.MovementType.VelocityTracking:
+                        attachTransform.SetPositionAndRotation(interactableObject.Rigidbody.worldCenterOfMass, interactable.transform.rotation);
+                        break;
+                    case XRBaseInteractable.MovementType.Kinematic:
+                        attachTransform.SetPositionAndRotation(interactableObject.Rigidbody.worldCenterOfMass, interactable.transform.rotation);
+                        break;
+                    case XRBaseInteractable.MovementType.Instantaneous:
+                        attachTransform.SetPositionAndRotation(interactable.transform.position, interactable.transform.rotation);
+                        break;
+                }
+            }
+            
+            base.OnSelectEnter(interactable);
         }
     }
 }
