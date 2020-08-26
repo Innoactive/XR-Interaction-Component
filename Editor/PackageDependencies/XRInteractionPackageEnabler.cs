fb851d16ed2fe44e5d049797a5d0f12269a63a75
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-08-26 17:46:07 +0200

    chore: constraints are now defined in the assembly

--- Editor/PackageDependencies/XRInteractionPackageEnabler.cs
+++ Editor/PackageDependencies/XRInteractionPackageEnabler.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using UnityEditor;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace Innoactive.CreatorEditor.PackageManager.XRInteraction
+﻿namespace Innoactive.CreatorEditor.PackageManager.XRInteraction
 {
     /// <summary>
     /// Adds Unity's XR-Interaction-Toolkit package as a dependency and sets specified symbol for script compilation.
     /// </summary>
-    public class XRInteractionPackageEnabler : Dependency, IDisposable 
-    {
-        private const string CreatorXRInteractionSymbol = "CREATOR_XR_INTERACTION";
-        
+    public class XRInteractionPackageEnabler : Dependency 
+    {        
         /// <inheritdoc/>
         public override string Package { get; } = "com.unity.xr.interaction.toolkit";
 
@@ -20,30 +13,5 @@ namespace Innoactive.CreatorEditor.PackageManager.XRInteraction
         
         /// <inheritdoc/>
         protected override string[] Layers { get; } = {"XR Teleport"};
-
-        public XRInteractionPackageEnabler()
-        {
-            OnPackageEnabled += PostProcess;
-        }
-        
-        public void Dispose()
-        {
-            OnPackageEnabled -= PostProcess;
-        }
-
-        private void PostProcess(object sender, EventArgs e)
-        {
-            BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
-            BuildTargetGroup buildTargetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget);
-            List<string> symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup).Split(';').ToList();
-        
-            if (symbols.Contains(CreatorXRInteractionSymbol) == false)
-            {
-                symbols.Add(CreatorXRInteractionSymbol);
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, string.Join(";", symbols.ToArray()));
-            }
-            
-            OnPackageEnabled -= PostProcess;
-        }
     }
 }
