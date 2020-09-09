52443930b77cd26fcaef7bedf83eb80c9431b5d6
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-09-09 08:50:20 +0200

    chore: cleaning

--- Runtime/Interaction/SnapZone.cs
+++ Runtime/Interaction/SnapZone.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
 using Innoactive.Creator.BasicInteraction.Validation;
-using NAudio.SoundFont;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
