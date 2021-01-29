74d7edc99ba9479361c3cce01564110aec15e29d
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2021-01-29 14:09:47 +0100

    fix: old xr setup is now deleted when setting the scene

--- Editor/XRInteractionSceneSetup.cs
+++ Editor/XRInteractionSceneSetup.cs
@@ -1,4 +1,6 @@
-﻿using Innoactive.CreatorEditor.BasicInteraction;
+﻿using UnityEditor;
+using UnityEngine;
+using Innoactive.CreatorEditor.BasicInteraction;
 
 namespace Innoactive.CreatorEditor.XRInteraction
 {
@@ -7,6 +9,9 @@ namespace Innoactive.CreatorEditor.XRInteraction
     /// </summary>
     public class XRInteractionSceneSetup : InteractionFrameworkSceneSetup
     {
+        private const string Title = "Obsolete XR Ring detected";
+        private const string Message = "Creator changed the Rig loading to a new dynamic system, you still have the old XR_Setup in the current scene, do you want to delete it?";
+        
         /// <inheritdoc />
         public override void Setup()
         {
@@ -15,7 +20,19 @@ namespace Innoactive.CreatorEditor.XRInteraction
         
         private void SetupXR()
         {
+            GameObject oldRig = GameObject.Find("[XR_Setup]");
+            
+            if (oldRig != null)
+            {
+                if (EditorUtility.DisplayDialog(Title, Message, "Delete", "Skip"))
+                {
+                    EditorUtility.SetDirty(oldRig);
+                    Object.DestroyImmediate(oldRig);
+                }
+            }
+            
             RemoveMainCamera();
+            
 #if ENABLE_INPUT_SYSTEM && XRIT_0_10_OR_NEWER
             SetupPrefab("[XR_Setup_Action_Based]");
 #else
