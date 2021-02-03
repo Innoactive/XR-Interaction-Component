2ac22d25c5c5562b590fa0fb9b3a0db04f5b9e36
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2021-02-03 09:30:33 +0100

    fix: wip

---
+++ Editor/SceneSetup/XRSimulatorImporter.cs
@@ -0,0 +1,87 @@
+﻿#if XRIT_1_0_OR_NEWER
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Innoactive.Creator.Core.Configuration;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Innoactive.CreatorEditor.PackageManager.XRInteraction
+{
+    /// <summary>
+    /// Utility class that ensures all Creator's prefabs are properly setup in the project.
+    /// </summary>
+    [InitializeOnLoad]
+    internal static class XRSimulatorImporter
+    {
+        private const string SimulatorPrefabName = "XR Device Simulator";
+        private const string ActionRigName = "[XR_Setup_Action_Based]";
+        private const string PrefabPath = "Assets/Resources/[XR_Simulator_Setup].prefab";
+
+        static XRSimulatorImporter()
+        {
+            if (AssetDatabase.GetMainAssetTypeAtPath(PrefabPath) == null)
+            {
+                DependencyManager.OnPostProcess += OnPostProcess;
+            }
+        }
+
+        private static void OnPostProcess(object sender, DependencyManager.DependenciesEnabledEventArgs e)
+        {
+            DependencyManager.OnPostProcess -= OnPostProcess;
+            ImportSimulatorRig();
+        }
+        
+        private static void ImportSimulatorRig()
+        {
+            GameObject simulator = LoadPrefab(SimulatorPrefabName, "Samples", out string simulatorPath);
+            GameObject actionRig = LoadPrefab(ActionRigName, "Innoactive", out string actionRigPath);
+
+            if (simulator == null || actionRig == null)
+            {
+                Debug.Log($"Simulator: {simulator == null}, Action Rig: {actionRig == null}");
+                return;
+            }
+
+            string resourcesPath = Path.GetDirectoryName(PrefabPath);
+            
+            if (Directory.Exists(resourcesPath) == false)
+            {
+                Directory.CreateDirectory(resourcesPath);
+            }
+
+            simulator.transform.SetParent(actionRig.transform);
+            PrefabUtility.SaveAsPrefabAsset(actionRig, PrefabPath);
+        }
+        
+        private static GameObject LoadPrefab(string prefabName, string searchFolder, out string assetPath)
+        {
+            string filter = $"t:Prefab {prefabName}";
+            string[] prefabsGUIDs = AssetDatabase.FindAssets(filter, new[] {$"Assets/{searchFolder}"});
+
+            foreach (string prefabGUID in prefabsGUIDs)
+            {
+                assetPath = AssetDatabase.GUIDToAssetPath(prefabGUID);
+
+                // GameObject runtimeConfigurator = new GameObject().AddComponent<RuntimeConfigurator>().gameObject;
+                // Scene roomPreview = EditorSceneManager.NewPreviewScene();
+                //
+                // SceneManager.MoveGameObjectToScene(runtimeConfigurator, roomPreview);
+                // runtimeConfigurator.AddComponent<RuntimeConfigurator>();
+                //
+                // PrefabUtility.LoadPrefabContentsIntoPreviewScene(assetPath, roomPreview);
+                // return roomPreview.GetRootGameObjects().First(g => g.name == prefabName);
+                
+                return PrefabUtility.LoadPrefabContents(assetPath);
+            }
+
+            assetPath = string.Empty;
+            return null;
+        }
+    }
+}
+#endif
---
+++ Runtime/Rigs/XRSimulatorSetup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Innoactive.Creator.Components.Runtime.Rigs
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class XRSimulatorSetup : XRSetupBase
+    {
+        /// <inheritdoc />
+        public override string Name { get; } = "XR Simulator";
+        
+        /// <inheritdoc />
+        public override string PrefabName { get; } = "[XR_Simulator_Setup]";
+
+        /// <inheritdoc />
+        public override bool CanBeUsed()
+        {
+#if ENABLE_INPUT_SYSTEM && XRIT_1_0_OR_NEWER
+            return IsEventManagerInScene() == false;
+#else
+            return false;
+#endif
+        }
+        
+        /// <inheritdoc />
+        public override string GetSetupTooltip()
+        {
+#if ENABLE_INPUT_SYSTEM
+            return "Can't be used while there is already a XRInteractionManager in the scene.";
+#else
+            return "Enable the new input system to allow using this rig.";
+#endif
+        }
+    }
+}
