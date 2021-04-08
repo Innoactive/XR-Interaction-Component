302dd124221aeb2d9e94e2bb72963eab51ba1e90
TomWim <samo.miw@gmail.com>
2021-04-08 17:08:13 +0200

    fix: make sure xr is loaded on scene load

--- Runtime/Rigs/XRSetupBase.cs
+++ Runtime/Rigs/XRSetupBase.cs
@@ -1,6 +1,7 @@
 ﻿using Innoactive.Creator.BasicInteraction.RigSetup;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Management;
 
 namespace Innoactive.Creator.Components.Runtime.Rigs
 {
@@ -13,7 +14,19 @@ namespace Innoactive.Creator.Components.Runtime.Rigs
             IsPrefabMissing = Resources.Load(PrefabName) == null;
             Resources.UnloadUnusedAssets();
         }
-        
+
+        /// <summary>
+        /// Makes sure the XR loader and subsystem is set up.
+        /// </summary>
+        public override void PreSetup()
+        {
+            if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+            {
+                XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
+                XRGeneralSettings.Instance.Manager.StartSubsystems();
+            }
+        }
+
         protected bool IsEventManagerInScene()
         {
             return Object.FindObjectOfType<XRInteractionManager>() != null;
